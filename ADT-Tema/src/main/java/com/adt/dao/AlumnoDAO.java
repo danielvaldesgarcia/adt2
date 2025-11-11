8d1b56ebb2a12fb9d1e889f5a687e80591e64910
danielvga16 <danielvga16@educastur.es>
2025-11-11 16:59:49 +0100

    tercer comit(testsAlumnodao)

--- ADT-Tema/src/main/java/com/adt/dao/AlumnoDAO.java
+++ ADT-Tema/src/main/java/com/adt/dao/AlumnoDAO.java
@@ -1,43 +1,129 @@
 package com.adt.dao;
 
 import com.adt.model.Alumno;
+import com.adt.model.Alumno;
+import com.adt.model.Alumno;
+import com.adt.model.Alumno;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
    public class AlumnoDAO {
 
 
-    private Connection conexion;
+       private Connection conexion;
 
-    public AlumnoDAO(Connection conexion) {
-        this.conexion = conexion;
-    }
+       public AlumnoDAO(Connection conexion) {
+           this.conexion = conexion;
+       }
 
-    public List<Alumno> listar() throws SQLException {
+       public List<Alumno> listar() throws SQLException {
 
-        List<Alumno> lista = new ArrayList<>();
-        String sql = "SELECT * FROM alumno";
-        Statement st = conexion.createStatement();
-        ResultSet rs = st.executeQuery(sql);
-        while(rs.next()){
-            int idAlumno = rs.getInt("id_alumno");
+           List<Alumno> lista = new ArrayList<>();
+           String sql = "SELECT * FROM alumno";
+           Statement st = conexion.createStatement();
+           ResultSet rs = st.executeQuery(sql);
+           while (rs.next()) {
+               int idAlumno = rs.getInt("id_alumno");
 
-            String nombreAlumno = rs.getString("nombre");
+               String nombreAlumno = rs.getString("nombre");
 
-            String apellidos = rs.getString("apellidos");
+               String apellidos = rs.getString("apellidos");
 
-            String email = rs.getString("email");
+               String email = rs.getString("email");
 
 
-            String curso = rs.getString("curso");
-            Alumno alumno = new Alumno(idAlumno,nombreAlumno,apellidos,email,curso);
-            lista.add(alumno);
-        }
-        return lista;
-    }
-}
+               String curso = rs.getString("curso");
+               Alumno alumno = new Alumno(idAlumno, nombreAlumno, apellidos, email, curso);
+               lista.add(alumno);
+           }
+           return lista;
+       }
+
+
+       public int insertar(Alumno alumno) throws SQLException {
+           String sql = "INSERT INTO alumno(nombre,apellidos,email,curso) " + "VALUES (?,?,?,?)";
+           PreparedStatement ps = conexion.prepareStatement(sql);
+           ps.setString(1, alumno.getNombre());
+           ps.setString(2, alumno.getApellidos());
+           ps.setString(3, alumno.getEmail());
+           ps.setString(4, alumno.getCurso());
+           int registrosAfectados = ps.executeUpdate();
+           return registrosAfectados;
+       }
+
+
+       public Alumno buscarPorId(int id) throws SQLException {
+           String sql = "SELECT * FROM alumno WHERE id_alumno = ?";
+           PreparedStatement ps = conexion.prepareStatement(sql);
+           ps.setInt(1, id); // primer parametro ?
+           ResultSet rs = ps.executeQuery();
+           Alumno alumno = null;
+           while (rs.next()) {
+               int idAlumno = rs.getInt("id_alumno");
+               System.out.println(idAlumno);
+               String nombreAlumno = rs.getString("nombre");
+               System.out.println(nombreAlumno);
+               String apellidos = rs.getString("apellidos");
+               System.out.println(apellidos);
+               String email = rs.getString("email");
+               String curso = rs.getString("curso");
+               alumno = new Alumno(idAlumno, nombreAlumno, apellidos, email, curso);
+
+           }
+           return alumno;
+       }
+
+       public int actualizar(Alumno alumno) throws SQLException {
+
+           String sql = "UPDATE alumno SET nombre=?, apellidos=?,email=?,curso=? " + "WHERE id_alumno=?";
+           PreparedStatement ps = conexion.prepareStatement(sql);
+           ps.setString(1, alumno.getNombre());
+           ps.setString(2, alumno.getApellidos());
+           ps.setString(3, alumno.getEmail());
+           ps.setString(4, alumno.getCurso());
+           ps.setInt(5, alumno.getIdAlumno());
+           int registrosAfectados = ps.executeUpdate();
+           return registrosAfectados;
+       }
+
+       public int eliminar(int id) throws SQLException {
+
+           String sql = "DELETE FROM alumno WHERE id_alumno = ?";
+           PreparedStatement ps = conexion.prepareStatement(sql);
+           ps.setInt(1, id);
+           int registrosAfectados = ps.executeUpdate();
+           return registrosAfectados;
+
+       }
+
+       public List<Alumno> consultarAlumnosPorSector(String curso) throws SQLException {
+           List<Alumno> alumnos = new ArrayList<>();
+           String sql = "select * from alumno where curso = ?";
+           PreparedStatement ps = conexion.prepareStatement(sql);
+           ps.setString(1, curso); // primer parametro ?
+           ResultSet rs = ps.executeQuery();
+           Alumno alumno = null;
+           while (rs.next()) {
+               alumno = new Alumno(
+                       rs.getInt("id_alumno"),
+
+                       rs.getString("nombre"),
+
+                       rs.getString("apellidos"),
+
+                       rs.getString("email"),
+
+                       rs.getString("curso")
+
+
+               );
+               alumnos.add(alumno);
+
+           }
+           return alumnos;
+       }
+
+
+   }
--- ADT-Tema/src/test/java/com/adt/dao/AlumnoDAOTest.java
+++ ADT-Tema/src/test/java/com/adt/dao/AlumnoDAOTest.java
@@ -2,8 +2,10 @@ package com.adt.dao;
 
 import com.adt.ConexionBD;
 import com.adt.model.Alumno;
+import com.adt.model.Empresa;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -13,8 +15,45 @@ class AlumnoDAOTest {
 
     @Test
     void listar() throws SQLException, ClassNotFoundException {
-        AlumnoDAO dao = new AlumnoDAO(ConexionBD.conectar());
-        List<Alumno> alumnos = dao.listar();
+        Connection conexion = ConexionBD.conectar();
+        AlumnoDAO alumnoDAO = new AlumnoDAO(conexion);
+        List<Alumno> listaEmpresas = alumnoDAO.listar();
+        System.out.println(listaEmpresas);
+    }
+
+    @Test
+    void insertar() throws SQLException, ClassNotFoundException {
+        AlumnoDAO alumnoDAO = new AlumnoDAO(ConexionBD.conectar());
+        Alumno alumno = new Alumno(8,"Julian SL","valdes","julian@julian.com","3");
+        int registros = alumnoDAO.insertar(alumno);
+        System.out.println(registros);
+    }
+
+    @Test
+    void buscarPorId() throws SQLException, ClassNotFoundException {
+        Connection connection = ConexionBD.conectar();
+        AlumnoDAO alumnoDAO = new AlumnoDAO(connection);
+        Alumno alumno = alumnoDAO.buscarPorId(3);
+        System.out.println(alumno);
+    }
+
+    @Test
+    void actualizar() throws SQLException, ClassNotFoundException {
+        AlumnoDAO alumnoDAO = new AlumnoDAO(ConexionBD.conectar());
+        Alumno alumno = new Alumno(2,"Julia","ramirez","7@gmail","3");
+        alumnoDAO.actualizar(alumno);
+    }
+
+    @Test
+    void eliminar() throws SQLException, ClassNotFoundException {
+        AlumnoDAO alumnoDAO = new AlumnoDAO(ConexionBD.conectar());
+        alumnoDAO.eliminar(3);
+    }
+
+    @Test
+    void consultarAlumnosPorSector() throws SQLException, ClassNotFoundException {
+        AlumnoDAO alumnoDAO = new AlumnoDAO(ConexionBD.conectar());
+        List<Alumno> alumnos = alumnoDAO.consultarAlumnosPorSector("3");
         System.out.println(alumnos);
     }
 }
